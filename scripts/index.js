d979f630a6d7e68aca2987f61d12e4df3024d43c
yannngithub <97786441+yannngithub@users.noreply.github.com>
2022-02-01 03:20:32 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
+  birthdate: 'Jul 17, 2022',
   name: 'Darlene'
 };
 
